bb0201238cd86808183f9931f620a131295546b9
janderson-js <jandersonsouzagit@gmail.com>
2022-05-04 16:17:39 -0300

    intial commit the project

---
+++ treino/src/main/java/br/com/bean/PersonBean.java
@@ -0,0 +1,87 @@
+package br.com.bean;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.dao.DAOGeneric;
+import br.com.entities.Person;
+
+@ManagedBean(name = "personBean")
+@ViewScoped
+public class PersonBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Person person = new Person();
+	
+	private List<Person> people = new ArrayList<Person>();
+	
+	private DAOGeneric<Person> daoGeneric = new DAOGeneric<Person>();
+	
+	public Person getPerson() {
+		return person;
+	}
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+	
+	
+	public void messages(String message, String labelName) {
+		FacesContext.getCurrentInstance().addMessage(labelName, new FacesMessage(message));
+	}
+	
+	public String toClear() {
+		person = new Person();
+		
+		return "";
+	}
+	
+	
+	public String toSave() throws IOException {
+		
+		try {
+			if(person.getId() != null) {
+				messages("Editado com sucesso!!", "toSaveMessage");
+			}else {
+				messages("Cadastrado com sucesso!!", "toSaveMessage");
+			}
+			person = daoGeneric.toMerge(person); 
+			listPerson();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if(person.getId() != null) {
+				messages("Ocorreu um erro ao editar o usúario!!","toSaveMessage");
+			}else {
+				messages("Ocorreu um erro ao cadastra o usúario!!","toSaveMessage");
+			}
+			listPerson();
+		}
+		
+		return "";
+	}
+	@PostConstruct
+	public void listPerson() {
+		people = daoGeneric.listPeopleDAO(person);
+	}
+	
+	public Person toEdit() {
+		
+		
+		return person;
+	}
+	public List<Person> getPeople() {
+		return people;
+	}
+	public void setPeople(List<Person> people) {
+		this.people = people;
+	}
+
+}
